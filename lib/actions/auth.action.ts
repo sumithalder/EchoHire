53877de85892eddf27296b11b4be334092491985
Sumit Halder <sumithalder100@gmail.com>
2026-01-22 23:14:46 +0530

    implement auth and storage

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -22,6 +22,11 @@ export async function signUp(params: SignUpParams) {
             name, email
         })
 
+        return {
+            success: true,
+            message: 'Account created successfully. Please sign in.'
+        }
+
     } catch (e: any) {
         console.error('Error creating a user', e);
 
@@ -78,4 +83,39 @@ export async function setSessionCookie(idToken: string) {
         path: '/',
         sameSite: 'lax'
     })
+}
+
+export async function getCurrentuser(): Promise<User | null> {
+    const cookieStore = await cookies();
+
+    const sessionCookie = cookieStore.get('session')?.value;
+
+    if(!sessionCookie) return null;
+
+    try {
+        const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+        const userRecord = await db.
+            collection('users')
+            .doc(decodedClaims.uid)
+            .get();
+
+        if(!userRecord.exists) return null;
+
+        return {
+            ...userRecord.data(),
+            id: userRecord.id,
+        } as User;
+
+    } catch(e) {
+        console.log(e);
+
+        return null;     
+    }
+}
+
+export async function isAuthenticated() {
+    const user = await getCurrentuser();
+
+    return !!user;
 }
